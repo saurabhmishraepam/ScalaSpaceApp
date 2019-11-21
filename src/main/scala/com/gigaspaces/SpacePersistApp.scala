c6b450364d58e6524aacc7d4f3864b426b8e3e81
Saurabh Mishra <Saurabh_Mishra@epam.com>
2019-11-21 16:37:01 +0530

    wan

--- src/main/scala/com/gigaspaces/SpacePersistApp.scala
+++ src/main/scala/com/gigaspaces/SpacePersistApp.scala
@@ -1,5 +1,8 @@
 package com.gigaspaces
 
+import java.io.{File, FileInputStream}
+import java.util.Properties
+
 import com.gigaspaces.config.SpaceConfig
 
 class SpacePersistApp {
@@ -7,6 +10,18 @@ class SpacePersistApp {
 }
 object SpacePersistApp {
   def main(args: Array[String]): Unit = {
+  var configFileName: String ="";
+    if (args.length != 0) {
+      configFileName = args(0)
+      var f = new File(configFileName)
+      if (f.exists()) {
+        println("found file")
+        val properties: Properties = new Properties()
+        properties.load(new FileInputStream(f))
+        val hostName = properties.get("host-name")
+        println(hostName)
+      }
+    }
     val space=new SpaceConfig().getGigaSpace
     val p2= new ProductV1("4", "productOne")
     space.write(p2)
--- src/main/scala/com/gigaspaces/config/SpaceConfig.scala
+++ src/main/scala/com/gigaspaces/config/SpaceConfig.scala
@@ -1,11 +1,18 @@
 package com.gigaspaces.config
 
+import java.util.Properties
+
 import org.openspaces.core.space.SpaceProxyConfigurer
 import org.openspaces.core.{GigaSpace, GigaSpaceConfigurer}
 
 class SpaceConfig {
 
   def getGigaSpace : GigaSpace ={
+
+    val url = getClass.getResource("src\\main\\resources\\application.properties")
+    val properties: Properties = new Properties()
+    val hostName = properties.get("host-name")
+    println(hostName)
     val space = new GigaSpaceConfigurer(new SpaceProxyConfigurer("demo").versioned(true)
       .lookupLocators("EPINHYDW0391")).gigaSpace //automatically it connects to localhost
      space
