2da0a88346e9dacdf4b724bd05b3c262a6b5f5d3
Saurabh Mishra <Saurabh_Mishra@epam.com>
2019-12-05 21:03:25 +0530

    updated code

--- src/main/java/com/gigaspaces/TestJust.java
+++ src/main/java/com/gigaspaces/TestJust.java
@@ -10,7 +10,7 @@ public class TestJust {
     public static void main(String [] args){
 
         GigaSpace gigaSpace = new GigaSpaceConfigurer(new SpaceProxyConfigurer
-                ("wanSpaceUS").lookupGroups("xap-15.0.0").lookupLocators("a5d95f54d176111eab4970255fbf9aaa-899163231.us-east-2.elb.amazonaws.com:4174")).gigaSpace();
+                ("wanSpaceDE").lookupGroups("xap-15.0.0").lookupLocators("a7939aaca176411ea988302c7a346453-215909573.us-east-2.elb.amazonaws.com:4174")).gigaSpace();
 
         System.out.println(gigaSpace.getClustered());
 
